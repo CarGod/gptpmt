c58310f3e989bd1ed2d547fa75e62dbf0b5b20d2
秋名山车神 <meteorshield@gmail.com>
2023-07-02 19:23:58 +0800

    Bugfix Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,24 @@ import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 import './style.css';
- 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5396QLYZJ2"></script>
-<script>
-{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-5396QLYZJ2');
-`}
-</script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5396QLYZJ2" />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5396QLYZJ2');
+        `}
+      </Script>
 
       <Component {...pageProps} />
       <Analytics />
     </>
   );
 }
- 
+
 export default MyApp;
